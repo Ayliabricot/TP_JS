c33f3a20e7762046b2da064c5ba8672c341911aa
Ayliabricot <you@example.com>
2026-02-04 11:52:08 +0100

    TP6

--- Cours_JS/2-Les_variables/monscript.js
+++ Cours_JS/2-Les_variables/monscript.js
@@ -1,19 +1,43 @@
-let nomJour = "Mercredi";
-let numJour = 4;
-let nomMois = "Février";
-let isHiver = true;
+// let nomJour = "Mercredi";
+// let numJour = 4;
+// let nomMois = "Février";
+// let isHiver = true;
 
-let tabJours = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"]; 
-let tabMois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+// let tabJours = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"]; 
+// let tabMois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 
-let maDateV1 = {
-    "jour" : tabJours[2],
-    "mois" : tabMois[1]
-}
+// let maDateV1 = {
+//     "jour" : tabJours[2],
+//     "mois" : tabMois[1]
+// }
 
-let maDateV2={};
-maDateV2.jour=tabJours[2];
-maDateV2.mois=tabMois[1];
+// let maDateV2={};
+// maDateV2.jour=tabJours[2];
+// maDateV2.mois=tabMois[1];
 
-console.log("Date 1 : "+maDateV1.jour,  numJour, maDateV1.mois, isHiver);
-console.log("Date 2 : "+maDateV2.jour,  numJour, maDateV2.mois, isHiver);
+let nbr1;
+let nbr2;
+let nbr3;
+let nbr4;
+
+nbr1 = 3;
+nbr2 = 7;
+nbr3 = 8;
+nbr4 = 21;
+
+// console.log("Date 1 : ",maDateV1.jour,  numJour, maDateV1.mois, isHiver);
+// console.log("Date 2 : ",maDateV2.jour,  numJour, maDateV2.mois, isHiver);
+
+console.log(nbr1, nbr2, nbr3, nbr4);
+console.log(nbr1+nbr2);
+console.log(nbr4-nbr3);
+
+nbr2 = nbr2*nbr1;
+
+console.log(nbr2);
+
+nbr2++;
+
+console.log(nbr2);
+console.log(nbr4/nbr1);
+console.log(nbr4%nbr1);
