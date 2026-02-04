fa53ad7d98f483f0bbeb8f70c2b8c1673ea8ac1b
Ayliabricot <you@example.com>
2026-02-04 12:01:59 +0100

    TP8

---
+++ Cours_JS/3-Les_conditions/monscript.js
@@ -0,0 +1,10 @@
+let number = 5;
+let text = '5';
+let isRainingToday = true;
+
+console.log("Double == :", number==text);
+console.log("Triple === :", number===text);
+
+if (isRainingToday===true){
+    console.log("Where is my umbrella?");
+}
