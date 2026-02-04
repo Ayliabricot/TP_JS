550cf9adebad9319c1f23c77e7d03ac7d1307db2
Ayliabricot <you@example.com>
2026-02-04 11:39:10 +0100

    TP5

--- Cours_JS/2-Les_variables/monscript.js
+++ Cours_JS/2-Les_variables/monscript.js
@@ -1,5 +1,19 @@
 let nomJour = "Mercredi";
-let numJour = 1;
-let nomMois = "Janvier";
+let numJour = 4;
+let nomMois = "Février";
 let isHiver = true;
-console.log(nomJour,  numJour, nomMois, isHiver);
+
+let tabJours = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"]; 
+let tabMois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+
+let maDateV1 = {
+    "jour" : tabJours[2],
+    "mois" : tabMois[1]
+}
+
+let maDateV2={};
+maDateV2.jour=tabJours[2];
+maDateV2.mois=tabMois[1];
+
+console.log("Date 1 : "+maDateV1.jour,  numJour, maDateV1.mois, isHiver);
+console.log("Date 2 : "+maDateV2.jour,  numJour, maDateV2.mois, isHiver);
