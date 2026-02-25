35a117b0b47d1a14503f2dea0884cd98cc7abfa8
Ayliabricot <you@example.com>
2026-02-25 11:39:59 +0100

    TP15

--- Cours_JS/6-Les_DOM/monscript.js
+++ Cours_JS/6-Les_DOM/monscript.js
@@ -1,9 +1,13 @@
 let li = document.querySelector("#listeCourses li");
+let dateJour = new Date(Date.now());
+let dateJourFr = dateJour.toLocaleDateString('fr-FR');
+let h2 = document.querySelector("h2");
 
-
+h2.textContent="Liste des courses : "+dateJourFr;
 li.addEventListener('click', ()=>{
   li.classList.toggle("itemCheck");
 })
+
 
 
 /* Les fonctions */
