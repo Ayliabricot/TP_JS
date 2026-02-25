10bfde68988c31778bc38191ce75fe8bb555e127
Ayliabricot <you@example.com>
2026-02-25 12:17:47 +0100

    TP16

--- Cours_JS/6-Les_DOM/monscript.js
+++ Cours_JS/6-Les_DOM/monscript.js
@@ -3,12 +3,37 @@ let dateJour = new Date(Date.now());
 let dateJourFr = dateJour.toLocaleDateString('fr-FR');
 let h2 = document.querySelector("h2");
 
+let myInput=document.querySelector("#myInput");;
+let btn=document.querySelector(".btn");;
+let liste=document.querySelector("ul");
+
+
 h2.textContent="Liste des courses : "+dateJourFr;
-li.addEventListener('click', ()=>{
-  li.classList.toggle("itemCheck");
+
+
+btn.addEventListener('click', ()=>{
+  addProduct();
 })
 
+myInput.addEventListener('keydown', (event)=> {
+  if (event.code === "Enter") {
+    addProduct();
+  }
+});
 
-
-/* Les fonctions */
-// A voir plus tard
+function addProduct(){
+    let value=myInput.value;
+    if (!value){
+        alert("Erreur de saisie");
+    }
+    else{
+        alert("Produit ajouté");
+        let newLi = document.createElement("li");
+        newLi.textContent = value;
+        newLi.addEventListener('click', ()=>{
+            newLi.classList.toggle("itemCheck");
+        })
+        liste.appendChild(newLi);
+        myInput.value="";
+    }
+}
