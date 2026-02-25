574bbf4c7a70b65cf09698de7cc4a847af2b9dd6
Ayliabricot <you@example.com>
2026-02-25 11:30:06 +0100

    TP13

---
+++ Cours_JS/6-Les_DOM/monscript.js
@@ -0,0 +1,11 @@
+let li = document.querySelector("#listeCourses li");
+
+
+li.addEventListener('click', ()=>{
+  li.style.backgroundColor="lightblue";
+  li.style.textDecoration="line-through";
+})
+
+
+/* Les fonctions */
+// A voir plus tard
