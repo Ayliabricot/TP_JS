1acf8e7704dfb25fe591f08d0e3ec17ea1aa3786
Ayliabricot <you@example.com>
2026-02-25 09:24:14 +0100

    TP11

--- Cours_JS/3-Les_conditions/monscript.js
+++ Cours_JS/3-Les_conditions/monscript.js
@@ -2,13 +2,31 @@ let age;
 
 age = prompt('Quel est ton âge ?');
 
-if (0<=age && age<18){
+
+if(age==18){
+    alert("L'utilisateur vient d'être majeur");
+}
+
+else if(age==25){
+    alert("L'utilisateur a un quart de siècle");
+}
+
+else if(age==50){
+    alert("L'utilisateur a un demi siècle");
+}
+
+else if(age==100){
+    alert("L'utilisateur vient d'être centenaire");
+}
+
+else if (0<=age && age<18){
     alert("L'utilisateur a "+age+" an(s), il est mineur!");
 }
+
 else if(0<=age && age<62){
     alert("L'utilisateur a "+age+" ans, il est majeur!");
 }
-else if(age>=62){
+else if(age>62){
     alert("L'utilisateur a "+age+" ans, il est majeur mais aussi retraité!");
 }
 else{
---
+++ Cours_JS/4-LesBoucles/monscript.js
@@ -0,0 +1,12 @@
+let somme = 0;
+let iterations = parseInt(prompt('Veuillez saisir un nombre entre 1 et 100 : '));
+if (!(iterations>=0) || !(iterations<=100)){
+    alert("La saisie n'est pas valide");
+}
+
+do{
+    somme+=iterations;
+    iterations--;
+}while(iterations>0);
+
+console.log(somme);
